6ce81f02c49873ff02d72276cbe4e69b4c3a8cd1
bonzoman <bonzoman@gmail.com>
2023-08-18 16:05:56 +0900

    aaa

---
+++ Lecture/Lec01.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract Lec01{
+    string public hi = "Hello solidity2";
+
+    bool public b1 = false;
+
+    bytes4 public bt4 = 0x12345678; //byte1~32
+    bytes public bt = "String";
+
+    address public addr = 0xE12EA99b2A6603EC19e3138a92d8F0101a588031;
+
+
+    int8 public i = 126; //int:음수포함
+    uint8 public ui = 123; //uint(unsigned int):음수포함X
+
+    uint256 public ether1 = 1 ether;
+    uint256 public wei1 = 1 wei;
+    uint256 public gwei1 = 1 gwei;
+    
+
+}
---
+++ Lecture/Lec02.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity >= 0.7.0 < 0.9.0; // ^0.8.17 : 0.8.17 이상
+
+/* 
+ * public, private, internal, external
+ *
+ * view : function 밖의 변수 Read만 가능
+ * pure : function 밖의 변수 Read, Write 모두 불가능
+ */
+
+contract Lec02 {
+    string public name;
+    uint public age;
+
+    constructor(string memory _name, uint _age) {
+        name = _name;
+        age = _age;
+    }
+
+
+    function setName(string memory _name) public {
+        name = _name;
+    }     
+
+    function getName() public view returns(string memory) {
+        return name;
+    }   
+
+
+    function setAge(uint _age) public {
+        age = _age;
+    } 
+
+    function getAge() public view returns(uint) {
+        return age;
+    }   
+}
+
+
+contract Lec02a{
+
+    Lec02 l02 = new Lec02("acucumy", 30);
+    
+    function setUserInfo(string memory _name, uint _age) public {
+        l02.setName(_name);
+        l02.setAge(_age);
+    } 
+    function getUserInfo() public view returns(string memory, uint){
+        return (l02.name(),l02.age());
+    } 
+    
+}
