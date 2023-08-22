a31b0e685188e679ddd2248439c16d8fd359d6f6
bonzoman <bonzoman@gmail.com>
2023-08-22 16:39:32 +0900

    sdfgsdfg

--- Lecture/Lec09(moneybox).sol
+++ Lecture/Lec09(moneybox).sol
@@ -21,14 +21,14 @@ contract MoneyBox {
     }
    
     receive() external payable {
-        require(msg.value == 0.0001 ether, "========>>>>>>>>> Must be 0.0001 ether.");
+        require(msg.value == 0.001 ether, "========>>>>>>>>> Must be 0.001 ether.");
         require(paidMemberList[round][msg.sender] == false, "========>>>>>>>>> Must be a new player in each game.");
         
         paidMemberList[round][msg.sender] = true;
         
         emit WhoPaid(msg.sender,msg.value);
         
-        if(address(this).balance == 0.0003 ether){
+        if(address(this).balance == 0.003 ether){
             (bool sent,)= payable(msg.sender).call{value:address(this).balance}("");
             require(sent,"========>>>>>>>>> Failed to pay");
             round++;
